4a9bb078777a7df3fd8fc89f5abcaa28fb8de2a6
Hünkar Döner <hunkardoner@gmail.com>
2025-05-22 21:59:11 +0300

    refactor: Wrap QueryClientProvider with SafeAreaProvider for improved layout handling

--- mobile/kupon-app/App.tsx
+++ mobile/kupon-app/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { StatusBar } from 'expo-status-bar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // Create a client
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
